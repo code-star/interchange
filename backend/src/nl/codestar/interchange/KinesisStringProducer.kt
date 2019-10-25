ed03f88c3c9b30051bd64d344fe40133d38167d4
Steven Vroonland <steven.vroonland@ordina.nl>
2019-10-25 15:08:36 +0200

    HTTP 1 connection

--- backend/src/nl/codestar/interchange/KinesisStringProducer.kt
+++ backend/src/nl/codestar/interchange/KinesisStringProducer.kt
@@ -3,10 +3,15 @@ package nl.codestar.interchange
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.AwsCredentials
 import software.amazon.awssdk.core.SdkBytes
+import software.amazon.awssdk.http.Protocol
+import software.amazon.awssdk.http.SdkHttpConfigurationOption
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
 import software.amazon.awssdk.services.kinesis.model.CreateStreamRequest
 import software.amazon.awssdk.services.kinesis.model.PutRecordRequest
+import software.amazon.awssdk.utils.AttributeMap
 import software.amazon.kinesis.common.KinesisClientUtil
 import java.net.URI
 import java.nio.charset.Charset
@@ -18,12 +23,28 @@ class KinesisStringProducer {
   private val partitionKey = "1"
   private val region = Region.of("eu-west-1")
   private val credentials = AwsBasicCredentials.create("test-id", "test-key")
+  val nettyClient: SdkAsyncHttpClient =
+    NettyNioAsyncHttpClient
+      .builder()
+      .protocol(Protocol.HTTP1_1)
+      .buildWithDefaults(
+        AttributeMap
+          .builder()
+          .put(
+            SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES,
+            java.lang.Boolean.TRUE
+          )
+          .build()
+      )
+
   private val builder = KinesisAsyncClient
     .builder()
     .region(this.region)
     .endpointOverride(URI.create("http://localhost:4567"))
     .credentialsProvider { credentials }
-  private val client = KinesisClientUtil.createKinesisAsyncClient(builder)
+    .httpClient(nettyClient)
+
+  private val client = builder.build()
 
   private fun createStream() {
     val request = CreateStreamRequest

4ccb9a2b57bbe21e59b516c056cac5a9d50193a9
Steven Vroonland <steven.vroonland@ordina.nl>
2019-10-25 15:31:17 +0200

    Fix consumer config

--- backend/src/nl/codestar/interchange/KinesisStringConsumer.kt
+++ backend/src/nl/codestar/interchange/KinesisStringConsumer.kt
@@ -1,10 +1,15 @@
 package nl.codestar.interchange
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.http.Protocol
+import software.amazon.awssdk.http.SdkHttpConfigurationOption
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
 import software.amazon.awssdk.services.kinesis.model.RegisterStreamConsumerRequest
 import software.amazon.awssdk.services.kinesis.model.SubscribeToShardRequest
+import software.amazon.awssdk.utils.AttributeMap
 import software.amazon.kinesis.common.KinesisClientUtil
 import java.net.URI
 import java.util.concurrent.TimeUnit
@@ -14,12 +19,29 @@ class KinesisStringConsumer {
   private val consumerName = "string-consumer-1"
   private val credentials = AwsBasicCredentials.create("test-id", "test-key")
   private val region = Region.of("eu-west-1")
+
+  val nettyClient: SdkAsyncHttpClient =
+    NettyNioAsyncHttpClient
+      .builder()
+      .protocol(Protocol.HTTP1_1)
+      .buildWithDefaults(
+        AttributeMap
+          .builder()
+          .put(
+            SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES,
+            java.lang.Boolean.TRUE
+          )
+          .build()
+      )
+    
   private val builder = KinesisAsyncClient
     .builder()
     .region(this.region)
     .endpointOverride(URI.create("http://localhost:4567"))
     .credentialsProvider { credentials }
-  private val client = KinesisClientUtil.createKinesisAsyncClient(builder)
+    .httpClient(nettyClient)
+
+  private val client = builder.build()
 
   fun run() {
     val request = RegisterStreamConsumerRequest

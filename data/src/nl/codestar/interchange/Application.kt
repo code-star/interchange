6bbc3522b744529df708f9687bb6f69a0fb713ba
Hamza Haiken <tenchi@team2xh.net>
2019-11-22 13:59:39 +0100

    Implement basic model with serializer (WIP)

--- data/src/nl/codestar/interchange/Application.kt
+++ data/src/nl/codestar/interchange/Application.kt
@@ -1,29 +1,9 @@
 package nl.codestar.interchange
 
-import com.typesafe.config.ConfigFactory
-import io.github.config4k.extract
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.locations.*
-import org.slf4j.event.*
-import io.ktor.websocket.*
-import io.ktor.http.cio.websocket.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.delay
-import kotlinx.serialization.Serializable
-import java.time.*
-
-import kotlinx.serialization.json.*
-import nl.codestar.interchange.here.HereRoute
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 import nl.codestar.interchange.here.HereRoutingAPIResponse
 
 
@@ -31,58 +11,34 @@ data class City(val name: String, val lattitude: String, val longitude: String)
 data class Edge(val start: City, val end: City)
 
 suspend fun main() {
-    fun CoroutineScope.produceNumbers() = produce<Int> {
-        var x = 1 // start from 1
-        while (true) {
-            send(x++) // produce next
-            delay(1000) // wait 0.1s
-        }
-    }
+    val appId = "knlxkDhhXNJfoE26rgIP"
+    val appCode = "j2VGkmwQ63x-BzKP_LOvyA"
 
-
-
-    val cities = mapOf(
-        "Amsterdam" to City("Amsterdam", "52.328084", "4.913361"),
-        "Utrecht" to City("Utrecht", "52.118121", "5.032961"),
-        "Rotterdam" to City("Rotterdam", "51.931298", "4.438818"),
-        "Groningen" to City("Groningen", "53.199353", "6.510003"),
-        "Nijmegen" to City("Nijmegen", "51.841687", "5.777498")
-    )
-
+    val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
+    val client = HttpClient()
     val graph = listOf(
-        Edge(cities.get("Amsterdam"), cities.get("Utrecht"))
+        Edge(
+            City("Amsterdam", "52.328084", "4.913361"),
+            City("Utrecht", "52.118121", "5.032961")
+        )
     )
 
     for (edge in graph) {
-        println(HereService.getRoutes(edge.start, edge.end))
+        val hereBytes = client.get<String>("https://route.api.here.com/routing/7.2/calculateroute.json" +
+                "?app_id=$appId" +
+                "&app_code=$appCode" +
+                "&waypoint0=geo!52.065465,5.0676533&waypoint1=geo!52.072752,4.902093&mode=shortest;car;traffic:disabled&alternatives=4&instructionFormat=text&representation=turnByTurn&jsonAttributes=25")
+
+        val hereJson = json.parse(HereRoutingAPIResponse.serializer(), hereBytes)
+
+        println(hereJson)
     }
 
 
+
+
+
+
 }
 
 
-object HereService {
-    val config = ConfigFactory.load()
-
-    val appId = config.extract<String>("here.appId")
-    val appCode = config.extract<String>("here.appCode")
-
-    private val client = HttpClient()
-    private val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
-
-    suspend fun getRoutes(start: City, end: City): List<HereRoute>? =
-        try {
-            val hereBytes = client.get<String>(
-                "https://route.api.here.com/routing/7.2/calculateroute.json" +
-                        "?app_id=$appId" +
-                        "&app_code=$appCode" +
-                        "&waypoint0=geo!${start.lattitude},${start.longitude}&waypoint1=geo!${end.lattitude},${end.longitude}" +
-                        "&mode=shortest;car;traffic:disabled&alternatives=4&instructionFormat=text&representation=turnByTurn&jsonAttributes=25"
-            )
-
-            json.parse(HereRoutingAPIResponse.serializer(), hereBytes).response.routes
-        } catch (_: Exception) {
-            null
-        }
-
-}
---
+++ data/test/nl/codestar/interchange/domain/GraphTest.kt
@@ -0,0 +1,47 @@
+package nl.codestar.interchange.domain
+
+import kotlinx.serialization.dumps
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlin.test.*
+
+class GraphTest {
+    val nodes = listOf(
+        Node(Position("50", "100")),
+        Node(Position("150", "200")),
+        Node(Position("250", "300"))
+    )
+
+    val edges = listOf(
+        Edge(nodes[0], nodes[1]),
+        Edge(nodes[0], nodes[2])
+    )
+
+    val graph = Graph(nodes, edges)
+
+    val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
+
+    @Test
+    fun testSerialization() {
+        val expected = """
+            {
+                "nodes": [
+                    {"position": {"latitude": "50", "longitude": "100"}},
+                    {"position": {"latitude": "150", "longitude": "200"}},
+                    {"position": {"latitude": "250", "longitude": "300"}}
+                ],
+                "edges": [
+                    {"a": 0, "b": 1},
+                    {"a": 0, "b": 2}
+                ]
+            }
+        """.trimIndent()
+
+        val serialized = json.toJson(Graph.serializer(), graph)
+
+        assertEquals(
+            json.parseJson(expected),
+            serialized
+        )
+    }
+}

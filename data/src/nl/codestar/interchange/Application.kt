2cb044fd654402608c9916e52e39ce97be3b4f6d
Vincent de Haan <vincentdehaan@gmail.com>
2019-11-22 16:19:17 +0100

    WIP. Not working

--- data/src/nl/codestar/interchange/Application.kt
+++ data/src/nl/codestar/interchange/Application.kt
@@ -1,37 +1,72 @@
 package nl.codestar.interchange
 
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.extract
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import nl.codestar.interchange.here.HereRoute
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import nl.codestar.interchange.domain.Edge
+import nl.codestar.interchange.domain.Graph
+import nl.codestar.interchange.domain.Node
+import nl.codestar.interchange.domain.Position
+
 import nl.codestar.interchange.here.HereRoutingAPIResponse
 
 
-data class City(val name: String, val lattitude: String, val longitude: String)
-data class Edge(val start: City, val end: City)
 
-suspend fun main() {
-    val appId = "knlxkDhhXNJfoE26rgIP"
-    val appCode = "j2VGkmwQ63x-BzKP_LOvyA"
+fun CoroutineScope.produceNumbers() = produce<Int> {
+    var x = 1 // start from 1
+    while (true) {
 
-    val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
-    val client = HttpClient()
-    val graph = listOf(
-        Edge(
-            City("Amsterdam", "52.328084", "4.913361"),
-            City("Utrecht", "52.118121", "5.032961")
+
+        for (edge in smallGraph().edges) {
+            println(HereService.getRoutes(edge.a.position, edge.b.position).toString().substring(0, 20))
+        }
+
+        send(3)
+        delay(3000) // wait 0.1s
+    }
+}
+
+val smallGraph = {
+    val amsterdam = Node(Position("52.328084", "4.913361"))
+    val utrecht = Node(Position("52.118121", "5.032961"))
+    val rotterdam = Node(Position("51.931298", "4.438818"))
+    val groningen = Node(Position("53.199353", "6.510003"))
+    val nijmegen = Node(Position("51.841687", "5.777498"))
+
+    Graph(
+        listOf(amsterdam, utrecht, rotterdam, groningen, nijmegen),
+        listOf(
+            Edge(amsterdam, utrecht),
+            Edge(utrecht, amsterdam),
+            Edge(amsterdam, rotterdam),
+            Edge(rotterdam, amsterdam),
+            Edge(rotterdam, utrecht),
+            Edge(utrecht, rotterdam),
+            Edge(groningen, utrecht),
+            Edge(utrecht, groningen),
+            Edge(utrecht, nijmegen),
+            Edge(nijmegen, utrecht),
+            Edge(nijmegen, groningen),
+            Edge(groningen, nijmegen)
         )
     )
+}
 
-    for (edge in graph) {
-        val hereBytes = client.get<String>("https://route.api.here.com/routing/7.2/calculateroute.json" +
-                "?app_id=$appId" +
-                "&app_code=$appCode" +
-                "&waypoint0=geo!52.065465,5.0676533&waypoint1=geo!52.072752,4.902093&mode=shortest;car;traffic:disabled&alternatives=4&instructionFormat=text&representation=turnByTurn&jsonAttributes=25")
+suspend fun main() = runBlocking<Unit>{
 
-        val hereJson = json.parse(HereRoutingAPIResponse.serializer(), hereBytes)
 
-        println(hereJson)
+    val producer = produceNumbers()
+
+    for( msg in producer) {
+        println(msg)
     }
 
 
@@ -42,3 +77,28 @@ suspend fun main() {
 }
 
 
+object HereService {
+    val config = ConfigFactory.load()
+
+    val appId = config.extract<String>("here.appId")
+    val appCode = config.extract<String>("here.appCode")
+
+    private val client = HttpClient()
+    private val json = Json(JsonConfiguration.Stable.copy(strictMode = false))
+
+    suspend fun getRoutes(start: Position, end: Position): List<HereRoute>? =
+        try {
+            val hereBytes = client.get<String>(
+                "https://route.api.here.com/routing/7.2/calculateroute.json" +
+                        "?app_id=$appId" +
+                        "&app_code=$appCode" +
+                        "&waypoint0=geo!${start.latitude},${start.longitude}&waypoint1=geo!${end.latitude},${end.longitude}" +
+                        "&mode=shortest;car;traffic:disabled&alternatives=4&instructionFormat=text&representation=turnByTurn&jsonAttributes=25"
+            )
+
+            json.parse(HereRoutingAPIResponse.serializer(), hereBytes).response.routes
+        } catch (_: Exception) {
+            null
+        }
+
+}

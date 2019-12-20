06fae59940e427d36acc16bf94692b010b36c287
Donovan de Kuiper <donovan.dekuiper@gmail.com>
2019-12-20 09:41:11 +0100

    Fixed build of data subproject

--- data/test/nl/codestar/interchange/ApplicationTest.kt
+++ data/test/nl/codestar/interchange/ApplicationTest.kt
@@ -1,17 +1,10 @@
 package nl.codestar.interchange
 
-import io.ktor.http.*
 import kotlin.test.*
-import io.ktor.server.testing.*
 
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
-            }
-        }
+
     }
 }

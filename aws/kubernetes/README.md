11baa15fde4f5cae155a6a943edd0a35b692343f
Jean-Marc van Leerdam <jean-marc.van.leerdam@ordina.nl>
2020-02-14 10:50:37 +0100

    Working Kustomize way to deploy and configure our app on the cluster

--- aws/kubernetes/README.md
+++ aws/kubernetes/README.md
@@ -4,3 +4,7 @@
 * make sure the Terraform stuff is finished (see `../interchange-module`)
 * make sure your CLI is configured to work with the EKS cluster (see `../README.md`)
 
+## Running the commands
+We use Kustomize to inject Terraform results into the kubernetes yamls.
+
+Usage: see `kbuild_eks.sh`
--- aws/kubernetes/kbuild_eks.sh
+++ aws/kubernetes/kbuild_eks.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 CLUSTER_IP=$(cd ../interchange-module;terraform output cluster_ip)
 CLUSTER_IP=${CLUSTER_IP,,}
-echo "Eventually we will use $CLUSTER_IP"
-sed "s|{{eks_hostname}}|${CLUSTER_IP}|g" eks/custom.json > eks/customTarget.json
-kubectl kustomize eks
+echo "Eventually we will use the hostname from $CLUSTER_IP"
+EKS_HOST=${CLUSTER_IP#"https://"}
+sed "s|{{eks_hostname}}|${EKS_HOST}|g" eks/custom.json > eks/customTarget.json
+kubectl kustomize eks > ../target/build.yaml
+kubectl apply -f ../target/build.yaml
